06637acfbfff3d3c45bd3299f9c952c8069b153e
Soumyattam Dey <soumyttamdey14@gmail.com>
2021-06-27 21:23:59 +0530

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Soumyattam Dey on 27/06/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
